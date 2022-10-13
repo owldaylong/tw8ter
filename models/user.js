f167037401c345660ac3b7a59de1c3b2ccb12741
ivanarifin <ivan.rarifin@gmail.com>
2022-10-13 11:22:18 +0700

    Models revisi

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+const hashPassword = require('../helpers/hashPassword');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -11,15 +12,62 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Post)
+      User.hasOne(models.Profile)
     }
   }
   User.init({
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.STRING
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {msg: 'Fill the username'},
+        notEmpty: {msg: 'Fill the username'},
+        max: {
+          args: [15],
+          msg: 'Maximum allowed characters are 15'
+        }
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {msg: 'Fill the email'},
+        notEmpty: {msg: 'Fill the email'},
+        isEmail: {msg: 'Please type in the correct email format'}
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {msg: 'Fill the password'},
+        notEmpty: {msg: 'Fill the password'},
+        max: {
+          args: [15],
+          msg: 'Maximum allowed password are 15'
+        },
+        min:{
+          args: [8],
+          msg: 'Minimum allowed password are 8'
+        }
+      },
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {msg: 'Pick the role'},
+        notEmpty: {msg: 'Pick the role'},
+      },
+    },
   }, {
     sequelize,
     modelName: 'User',
   });
+  User.beforeCreate((user) =>{
+    user.password = hashPassword(user.password)
+  })
   return User;
 };
